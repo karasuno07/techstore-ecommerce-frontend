c4f81f789449d9df61906fb57339a260817b481a
HaiTT <tranhai01se@gmail.com>
2021-11-16 11:35:47 +0700

    Add login page

---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./Homepage.module.scss"
+
+const Homepage = () => {
+   return <div className={styles.wrapper}>
+       <h1>Hai ne</h1>
+   </div>
+}
+
+export default Homepage
