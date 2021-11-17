7a82963bcf9ddbd1e5a01b42d500efff29cbdf2f
HaiTT <tranhai01se@gmail.com>
2021-11-17 19:00:55 +0700

    Add Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,34 @@
 import React from "react"
-import styles from "./Homepage.module.scss"
+import clsx from "clsx"
+import {Card} from "react-bootstrap"
+import stylesHome from "./Homepage.module.scss"
+import styles from "./Login.module.scss"
+
+import Input from "components/UI/Input"
+import Button from "components/UI/Button"
 
 const Homepage = () => {
-   return <div className={styles.wrapper}>
-       <h1>Hai ne</h1>
+   return <div className={clsx(stylesHome.wrapper,"pt-5")}>
+       <Card className={clsx(styles.card)}>
+            <div className="header">
+                <h2>Đăng ký</h2>
+            </div>
+            <div className="body">
+                <form action="" id="formLogin">
+                    <Input type="text" placeholder="Số điện thoại" className={clsx(styles.inputControl,"mt-3")}/>
+                    <Button />
+                </form>
+                <div>
+
+                </div>
+                <div className="orLogin">
+
+                </div>
+            </div>
+            <div className="footer">
+
+            </div>
+       </Card>
    </div>
 }
 
